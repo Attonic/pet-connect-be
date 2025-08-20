e3aca5e3002f688fa924ab346eebf0cf09ce9b85
AntonioCleison <a.cleisonn@gmail.com>
2025-08-20 11:57:08 -0300

    Add: Adiciona anotação @CrossOrigin para aceitar rotas externas

--- src/main/java/com/petconnectbe/controllers/UserController.java
+++ src/main/java/com/petconnectbe/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.petconnectbe.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
 
     private final UserService userService;
